666ac369a7cb1d1a1c53cabb5b89990f20d3492f
kadwalon LOBET <Kadwalon.lobet@orange.Fr>
2024-03-22 17:01:38 +0100

    Ajout Plateau

---
+++ plateau.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include "std.h"
+
+class Plateau {
+    //40 case
+    // case depart 
+    // case prison 
+private:
+    array circular_array ;
+public:
+// Constructeur
+Plateau();
+    
+};
