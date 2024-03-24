f535862f45da9e7919da02dff3d754948764d652
kadwalon LOBET <Kadwalon.lobet@orange.Fr>
2024-03-24 15:25:44 +0100

    Prototype plateau avec instance de Box

--- plateau.cpp
+++ plateau.cpp
@@ -1,1 +1,18 @@
-#include plateau.hpp
+#include "plateau.hpp"
+#include "box.hpp" // Supposons que la classe Box est défini dans un fichier box.hpp
+
+// Constructeur par défaut
+Plateau::Plateau() {
+    // Initialisation de chaque case du plateau avec des instances de Box
+    for (int i = 0; i < 40; ++i) {
+        plateau[i] = new Box(); // Vous devrez probablement définir un constructeur par défaut dans la classe Box
+    }
+}
+
+// Destructeur
+Plateau::~Plateau() {
+    // Libération de la mémoire allouée pour chaque case du plateau
+    for (int i = 0; i < 40; ++i) {
+        delete plateau[i];
+    }
+}
