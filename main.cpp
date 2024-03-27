755575c94905ddcc6f702cb7eb3212db56c517a6
kadwalon LOBET <Kadwalon.lobet@orange.Fr>
2024-03-27 11:30:08 +0100

    test fonctionnel de House et Hotel

--- main.cpp
+++ main.cpp
@@ -2,8 +2,21 @@
 #include "realEstateLand.hpp"
 #include "plateau.hpp"
 #include "box.hpp" // Supposons que vous avez défini la classe Box dans un fichier box.hpp
+#include "Hotel.hpp"
+#include <cassert>
 
 using namespace std;
+
+void testHotel() {
+    Hotel hotel;
+
+    // Test de la méthode getPrice()
+    assert(hotel.getPrice() == 250);
+
+    // Test de la méthode print()
+    std::cout << "Printing hotel details:" << std::endl;
+    hotel.print();
+}
 
 int main() {
     // Création d'un plateau
@@ -25,6 +38,11 @@ int main() {
     } else {
         cerr << "Erreur : indice de case invalide." << endl;
     }
+
+    // Exécuter les tests
+    testHotel();
+
+    std::cout << "All tests passed successfully!" << std::endl;
 
     return 0;
 }
