93e56ded8f45392938dad6cfa56326edc3dcde8e
kadwalon LOBET <Kadwalon.lobet@orange.Fr>
2024-03-27 09:18:07 +0100

    main test

--- main.cpp
+++ main.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
 #include "realEstateLand.hpp"
+#include "plateau.hpp"
+#include "box.hpp" // Supposons que vous avez défini la classe Box dans un fichier box.hpp
 
 using namespace std;
 
-int main()
-{
+int main() {
+    // Création d'un plateau
+    Plateau plateau;
+
     cout << "Bonjour monde!" << endl;
     RealEstateLand my_land("Mairie de doulon", 666, Color::GREEN);
     cout << my_land << endl;
+
+    // Affichage du plateau
+    std::cout << "Plateau avant initialisation :" << endl;
+    plateau.print();
+
+    // Accès à une case spécifique (par exemple, la case 5)
+    int index = 5;
+    Box* box = plateau.getBoxAt(index);
+    if (box != nullptr) {
+        plateau.printBoxAt(index);
+    } else {
+        cerr << "Erreur : indice de case invalide." << endl;
+    }
 
     return 0;
 }
