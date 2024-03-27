272fb491d89998b9fddfb4d3ead56cb98473ead1
Cédric FERRÉ <cedric.c.ferre@gmail.com>
2024-03-27 11:51:07 +0100

    Ajout dans la classe player

---
+++ player.cpp
@@ -0,0 +1,61 @@
+#include "player.hpp"
+
+Player::Player(string newName, int newFinancialAssets, int movement, int nbDouble, int position)
+{
+    this->name = newName;
+    this->financialAssets = newFinancialAssets;
+    this->movement = movement;
+    this->nbDouble = nbDouble;
+    this->position = position;
+}
+
+void Player::buyHouse()
+{
+
+}
+
+void Player::buyHotel()
+{
+
+}
+
+void Player::buyRealEstateLand()
+{
+
+}
+
+string Player::getName() const
+{
+    return this->name;
+}
+
+int Player::getFinancialAssets() const
+{
+    return this->financialAssets;
+}
+
+int Player::getMovement() const
+{
+    return this->movement;
+}
+
+int Player::getNbDouble() const
+{
+    return this->nbDouble;
+}
+
+int Player::getPosition () const
+{
+    return this->position;
+}
+
+// ostream& operator<<(ostream& os, const Player& player);
+// {
+//     os << "Nom du joueur :" <<player.getName() << endl;
+//     os << "     Patrimoine Financier : " <<player.getFinancialAssets() << endl;
+//     os << "     Résultat cumulé des dés : " <<player.getMovement() << endl;
+//     os << "     Nombre de doubles consécutifs : " <<player.getNbDouble() << endl;
+//     os << "     Position : " <<player.getPosition() << endl;
+
+//     return os;
+// }
---
+++ player.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+
+using namespace std;
+
+class Player
+{
+    private:
+
+        string name;
+        int financialAssets;
+        int movement;
+        int nbDouble;
+        int position;
+
+    public:
+
+        Player(string newName, int newFinancialAssets, int movement, int nbDouble, int position);
+
+        void buyHouse();
+        void buyHotel();
+        void buyRealEstateLand();
+
+        string getName() const;
+        int getFinancialAssets() const;
+        int getMovement() const;
+        int getNbDouble() const;
+        int getPosition () const;
+
+        friend ostream& operator<<(ostream& os, const Player& player);
+};
