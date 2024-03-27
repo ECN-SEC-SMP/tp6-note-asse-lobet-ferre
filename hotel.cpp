6f8be045ed10486a5b03daaab7b4c5b60d4548d7
kadwalon LOBET <Kadwalon.lobet@orange.Fr>
2024-03-27 11:29:33 +0100

    test fonctionnel de House et Hotel

--- hotel.cpp
+++ hotel.cpp
@@ -2,12 +2,22 @@
 
 #include <iostream>
 
+//Constructeur
+Hotel::Hotel() {
+    Hotel::PriceHotel = 250;
+}
+
+//Destructeur 
+Hotel::~Hotel() {
+}
+
+
 // Getter pour obtenir le prix d'une hotel
 int Hotel::getPrice() const {
     return Hotel::PriceHotel;
 }
 
-
+// Print d'un hotel
 void Hotel::print() const {
     std::cout << "Hotel" << std::endl;
     std::cout << "Price: " << getPrice() << std::endl;
--- house.cpp
+++ house.cpp
@@ -2,12 +2,21 @@
 
 #include <iostream>
 
+//Constructeur
+House::House() {
+    House::PriceHouse = 200;
+}
+
+//Destructeur 
+House::~House() {
+}
+
 // Getter pour obtenir le prix d'une hotel
 int House::getPrice() const {
     return House::PriceHouse;
 }
 
-
+//Print d'une maison
 void House::print() const {
     std::cout << "House" << std::endl;
     std::cout << "Price: " << getPrice() << std::endl;
